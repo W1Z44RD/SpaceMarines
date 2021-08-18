322299c0ff6bda771e14418f19bf6713c8f13ecf
Artem <artok25038@gmail.com>
2021-08-18 11:06:11 +0300

    added new tests, old optimised

--- src/test/java/com/example/SpaceMarines/integration/MarineControllerTest.java
+++ src/test/java/com/example/SpaceMarines/integration/MarineControllerTest.java
@@ -10,10 +10,12 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MarineControllerTest {
     @LocalServerPort
@@ -22,10 +24,12 @@ public class MarineControllerTest {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+    private HttpEntity<Marine> marineHttpEntity;
 
     @BeforeEach
     public void setup() {
         testRestTemplate.getRestTemplate().setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        marineHttpEntity = new HttpEntity<>(new Marine("Bob", "Private", 24));
     }
 
     @Test
@@ -35,14 +39,12 @@ public class MarineControllerTest {
 
     @Test
     public void addMarineTest() throws Exception{
-        HttpEntity<Marine> marineHttpEntity = new HttpEntity<>(new Marine("Bob", "Private", 24));
         this.testRestTemplate.postForEntity(url + port + "/marine/add", marineHttpEntity, Marine.class);
         assertThat(this.testRestTemplate.getForObject(url + port + "/marine/get/id/1", Marine.class)).toString().contains(marineHttpEntity.toString());
     }
 
     @Test
     public void deleteMarineTest() throws Exception{
-        HttpEntity<Marine> marineHttpEntity = new HttpEntity<>(new Marine("Bob", "Private", 24));
         this.testRestTemplate.postForEntity(url + port + "/marine/add", marineHttpEntity, Marine.class);
         this.testRestTemplate.delete(url + port + "/marine/delete/1");
         assertThat(this.testRestTemplate.getForObject(url + port + "/marine/get/id/1", String.class)).contains("null");
@@ -50,11 +52,15 @@ public class MarineControllerTest {
 
     @Test
     public void patchMarineTest() throws Exception{
-        HttpEntity<Marine> marineHttpEntity = new HttpEntity<>(new Marine("Bob", "Private", 24));
         this.testRestTemplate.postForEntity(url + port + "/marine/add", marineHttpEntity, Marine.class);
         assertThat(this.testRestTemplate.getForObject(url + port + "/marine/get/id/1", Marine.class)).toString().contains(marineHttpEntity.toString());
         HttpEntity<Marine> marineNewRank = new HttpEntity<>(new Marine("", "Lieutenant",0));
-        this.testRestTemplate.patchForObject(url + port + "/marine/patch/rank/2", marineNewRank, Marine.class);
-        assertThat(this.testRestTemplate.getForObject(url + port + "/marine/get/id/2", String.class)).contains("Lieutenant");
+        this.testRestTemplate.patchForObject(url + port + "/marine/patch/rank/1", marineNewRank, Marine.class);
+        assertThat(this.testRestTemplate.getForObject(url + port + "/marine/get/id/1", String.class)).contains("Lieutenant");
+    }
+
+    @Test
+    public void getAllTest() throws Exception{
+        assertThat(this.testRestTemplate.getForObject(url + port + "/marine/get/all", Object.class)).toString().contains(marineHttpEntity.toString());
     }
 }
